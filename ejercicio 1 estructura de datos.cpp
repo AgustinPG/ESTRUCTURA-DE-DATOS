dfde16ebd793b42904de4a156fd087083e1e4684
AgustinPG <95065094+AgustinPG@users.noreply.github.com>
2022-02-09 19:30:16 -0400

    Add files via upload

---
+++ ejercicio 1 estructura de datos.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include <iostream>
+#include "conio.h"
+
+using namespace std;
+int main(){
+
+	float L;
+
+	cout<<"intoduce el lado del cubo";cin>>L;
+			cout<<"el volumen del cubo es: "<<L*L*L<<endl;
+
+getch();
+	return 0;
+	
+
+}
+	 
+
+
+
